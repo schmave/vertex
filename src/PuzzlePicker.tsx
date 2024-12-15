63109c6e77472042954e1575ad5e217e7d2bee73
Evan Mallory <schmave@gmail.com>
2024-12-15 17:02:15 -0500

    Beginnings of puzzle history picker

--- src/PuzzlePicker.tsx
+++ src/PuzzlePicker.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { createGame } from "./main";
 import testPuzzle from "./test.json";
 import puzzleDates from "./puzzle-dates.json";
+import dayjs, { Dayjs } from "dayjs";
 
 type State = {
   selectedPuzzle?: string;
+  chosenYear: number;
 };
 
 type Props = {
@@ -15,6 +17,7 @@ export default class PuzzlePicker extends React.PureComponent<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
+      chosenYear: 2021,
       selectedPuzzle: undefined,
     };
   }
@@ -29,11 +32,34 @@ export default class PuzzlePicker extends React.PureComponent<Props, State> {
     this.props.onHide();
   };
 
+  getClosestPuzzle = () => {
+    let bestDiff = 10000000000,
+      best: Dayjs;
+
+    const target = dayjs().set("year", this.state.chosenYear);
+
+    for (const i in puzzleDates) {
+      const thisDate = dayjs(puzzleDates[i]);
+      if (thisDate.year() === this.state.chosenYear) {
+        const diff = Math.abs(thisDate.diff(target, "day"));
+        if (diff < bestDiff) {
+          bestDiff = diff;
+          best = thisDate;
+        }
+      }
+    }
+
+    return best;
+  };
+
   render() {
     return (
       <div id="overlay">
-        <div className="container">
+        <div className="puzzle-picker">
           <span>Vertex Archive</span>
+          <div>
+            Best puzzle is {this.getClosestPuzzle().format("MMMM D, YYYY")}
+          </div>
           <select
             name="puzzle"
             id="select"
