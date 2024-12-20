65bdab871aa1c455e802a3b2f6014c302bcf1728
Evan Mallory <schmave@gmail.com>
2024-12-20 15:17:49 -0500

    Make deploy script do everything

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 pnpm run build
 cd dist
@@ -6,3 +7,12 @@ tar --no-mac-metadata --no-xattrs -czf ../vertex.tgz *
 cd ..
 scp vertex.tgz evan@evanmallory.com:~/
 rm vertex.tgz
+
+ssh evan@evanmallory.com <<EOF
+set -ex
+cd evanmallory.com
+rm -rf vertex
+mkdir vertex
+cd vertex
+tar xzf ~/vertex.tgz
+EOF
