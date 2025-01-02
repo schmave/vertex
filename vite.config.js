96fc73e376a856a75ca7b0ae7ef079e7ae8e3cdb
Evan Mallory <schmave@gmail.com>
2025-01-02 11:56:39 -0500

    Build source map and serve it

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    sourcemap: 'true',
+  },
   envPrefix: ['ROLLBAR'],
   plugins: [react()],
 });
